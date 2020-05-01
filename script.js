86ccf4a9ecab290cec48ca0b3a82757e7b452bd7
Doris Castro <dorisgeo12@gmail.com>
2020-05-01 01:25:55 -0400

    adding homework 04 code quiz

--- script.js
+++ script.js
@@ -3,81 +3,89 @@
 
 var questions = [
   {
-    q1: "Commonly used data types DO NOT INCLUDE:",
-    a1: ["Strings", "Boolean", "Alerts", "Number"],
-    corIndex1: 2
+    q: "Commonly used data types DO NOT INCLUDE:",
+    a: ["Strings", "Boolean", "Alerts", "Number"],
+    corIndex: 2
   },
   {
-    q2: "The condition in an if/else statement is enclosed within",
-    a2: ["Quotes", "Curly Bracket", "Parentheses", "Square Bracket"],
-    corIndex2: 2
+    q: "The condition in an if/else statement is enclosed within",
+    a: ["Quotes", "Curly Bracket", "Parentheses", "Square Bracket"],
+    corIndex: 2
   },
   {
-    q3: "Array in Javascript and be used to store _____.",
-    a3: ["Numbers and Strings", "Booleans", "Other Arrays", "All of the above"],
-    corIndex3: 3
+    q: "Array in Javascript and be used to store _____.",
+    a: ["Numbers and Strings", "Booleans", "Other Arrays", "All of the above"],
+    corIndex: 3
   },
   {
-    q4: "String value must be encosed within _____ when being assigned to variables",
-    a4: ["Commas", "Curly Brackets", "Quotes", "Parentheses"],
-    corIndex4: 2
+    q: "String value must be encosed within _____ when being assigned to variables",
+    a: ["Commas", "Curly Brackets", "Quotes", "Parentheses"],
+    corIndex: 2
   },
   {
-    q5: "A very useful tool during development and debugging for printing content to the debugger is.",
-    a5: ["Console Log", "Terminal/Bash", "For Loop", "Javascript"],
-    corIndex5: 0
+    q: "A very useful tool during development and debugging for printing content to the debugger is.",
+    a: ["Console Log", "Terminal/Bash", "For Loop", "Javascript"],
+    corIndex: 0
   },
 ]
 
 
 var timerEl = document.getElementById("countdown");
-var quizStart = document.querySelector("btn");
+var quizStart = document.getElementById("start-quiz");
 var timeLeft = 75;
 var timeInterval;
 var index = 0;
 var titleQuestions = document.querySelector(".title");
 var quest = [questions[0].q1,questions[1].q2, questions[2].q3, questions[3].q4, questions[4].q5];
+var highScore = document.getElementById("Highscore");
+var introCard = document.querySelector(".card-body.intro");
 
 //var ans = [a1, a2, a3, a4, a5];
 //var correct = [corIndex1, corIndex2, corIndex3, corIndex4, corIndex5];
 
-document.addEventListener("click", function () {
+//document.write(highScore);
+
+quizStart.addEventListener("click", function () {
   // start the timer
   quizTimer()
+  introCard.style.display = "none"
   //showquestion
   showquestion()
-  console.log(showquestion);
 });
 
 function showquestion() {
-  // get for the arry the question in the index
-  for (var i = 0; i < questions.length; i++); { 
-    // questions.getElementById(".title");  
-  // appen the question
- //questions.append(titleQuestions);
-  // append the buttons
-  //ans.append(Buttons);
-  // append the onclick for the buttons  --->  every cick you verify the response if correct or not --- > increment --> verify if index is the last question --> 
-  //correct.append(answerQuestion())  
-}
+var currentQuestion= questions[index] //to keep of the current question
+titleQuestions.textContent = currentQuestion.q
+var answerArea = document.getElementById("answerArea")
 
-    //function answerQuestion() {
+  for(var i=0; i <currentQuestion.a.length; i++){
+  var button = document.createElement("button");
+  button.style.display = "block";
+  button.style.margin = "auto";
+  button.textContent = currentQuestion.a[i];  
+  answerArea.appendChild(button);
+ 
+  }
+   button.onclick = answerQuestion;
+//  append the onclick for the buttons  --->  every cick you verify the response if correct or not --- > increment --> verify if index is the last question --> 
+ 
+  function answerQuestion() {
+    
+var feedback = document.getElementById("feedback");
+ var time = questions.length * 10;
+ var choices = document.getElementById("choices");
 
-      //if (questions === true) {
-        //questions.length; index;
-      //}
-      //else (questions === false){
-     //   quizTimer();
-      //}
-   // }
+    if (this.value !== questions[index].answer) 
+      // penalize time
+      time -= 10;        
+      choices.innerHTML = "";  
+    }
     //  if last question the go to results  if  index < array.length the index and go to showquestion()
-
-
-    //loop into the array of a  to build the 3 o 4 buttons
-   var btn = document.createElement("button")
-  //for(i=0; i<a1.length;i++);
-    //btn.innerText = ""
-    //btn.value = 0
+    feedback.setAttribute("class", "feedback");
+    setTimeout(function () {
+      feedback.setAttribute("class", "feedback hide");
+    }, 1000);
+    //Index++;
 
   }
 function quizTimer() {
